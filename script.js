a89fa3b47eb7506d5237c2c64d9449373580914e
Svetlana Nesterova <sveta59n@yahoo.com>
2022-08-17 20:23:58 -0400

    create a password function

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
 // Assignment code here
 
 
+var chooseLength = Number(window.prompt("Please choose password length between 8 and 128 characters"));
+  
+
+console.log(chooseLength);
+
+  function generatePassword() {
+	var length = chooseLength;
+
+  if (chooseLength > 7 && chooseLength < 129) {
+
+  var passwordChosen = "";
+  
+  var passwordLower = window.confirm("Would you like to use LOWER case letters?");
+  charsetLower = "abcdefghijklmnopqrstuvwxyz"; 
+  if (passwordLower){
+    passwordChosen += charsetLower;     
+  }
+
+  var passwordUpper = window.confirm("Would you like to use UPPER case letters?");
+  if (passwordUpper) {
+    charsetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    passwordChosen += charsetUpper;;
+    
+  }
+
+  var passwordNumber = window.confirm("Would you like to use NUMBERS?");
+  if (passwordNumber){
+    charsetNumber = "0123456789";
+    passwordChosen += charsetNumber;
+    
+  }
+
+  var passwordSymbol = window.confirm ("Would you like to use SPECIAL CHARACTERS?");
+  if (passwordSymbol){
+    charsetSymbol = "!#$%&'()*+,-./:;<]=>?@^_`{|}~[";
+    passwordChosen += charsetSymbol;
+    
+  }
+
+  passwordFinal = "";
+  for (var i = 0; i < length; ++i) {
+		passwordFinal += passwordChosen.charAt(Math.floor(Math.random() * passwordChosen.length));
+	}
+	return passwordFinal;
+
+  
+
+} else {
+  window.alert("You did not choose a valid option");
+  
+}	
+};
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
