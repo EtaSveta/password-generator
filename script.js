353ad40e5535b0c849d4768a0ad42affb431da4c
Svetlana Nesterova <sveta59n@yahoo.com>
2022-08-19 19:50:39 -0400

    get length

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 // Assignment code here
 
 
-// A better way to write this program would be many smaller functions that way you would be able to ask for the length and what characters to use outside of the function and then it would all be able to work properly without errors with the window alerts
-
-// I believe that the characters are also supposed to be in an array rather than a string for this assignment
 
 
-// The way the assignment is supposed to be structured is to have multiple arrays of characters, then based on what characters the user chooses a new array is created by adding the individual arrays together. Then the generate password function should be called using the password length and the array of characters and that would generate the password correctly. All of the error checking with the window alerts for password length and character selection would be handled in the earlier functions to make sure the generate password function will work
-
-// does that structure make more sense? That is the structure of the intended solution to the assignment.
-
-// A cleaner solution is to break it up into individual functions for each or all of the if statements, then once you have what characters they would like, you can check to make sure they have characters selected, if they do not then you can restart from the password length function. Same goes for the Length function it would ask the length, if it is invalid then it would reset the function and ask for the length. Once you have the answer for how long the password should be and what characters the user would like then you will run the final generation function which will take the length of the password and all of the characters and create a password for the user.
-
-
-
-//That is the structure of the intended solution to the assignment.
-
-
-var chooseLength = parseInt(window.prompt("Please choose password length between 8 and 128 characters"));
+//var chooseLength = parseInt(window.prompt("Please choose password length between 8 and 128 characters"));
   
 console.log(chooseLength);
 
   function generatePassword() {
 	
-  var length = parseInt(window.prompt("Please choose password length between 8 and 128 characters")) {
-
-
-  while (isNaN(length) || length <8 || length > 128) 
+  var length = parseInt(window.prompt("Please choose password length between 8 and 128 characters"));
+  while (isNaN(length) || length <8 || length > 128) {
+    length = parseInt(window.prompt("Please choose password length between 8 and 128 characters"));
+  }
 }
   var passwordChosen = "";
   
